51277c2772c3ae3ded8122e658e14401aed0264b
Thomas GUERY <originaltompouce@gmail.com>
2026-04-11 18:31:38 +0200

    Add Slot Lock as a supported game (empty template)

---
+++ src/components/gamedata/GDataSlotLock.vue
@@ -0,0 +1,52 @@
+﻿<template>
+    <div class="inline-block">
+
+        <div :class="getImageClass()" class="inline-block bg-stone-100/40 rounded-xs p-[2px] pl-[4px] pb-[4px] mx-2 bg-opacity-25">
+            🧩
+
+        </div>
+    </div>
+</template>
+    
+<script>
+
+/**
+* JigSaw
+* 
+* Count puzzle pieces and win.
+*/ 
+export default {
+  name: "gDataSlotLock",
+        props: {
+            data: Object,
+            gamedata: Object,
+            index: Number,
+            checks_done: Number,
+            total_checks: Number,
+            player_name: String,
+            player_game: String
+  },
+  data: function () {
+    return {
+    };
+  },
+
+        methods: {
+            getGoalDetails: function () {
+                if (!this.$parent.hasSlotData())
+                    return [];
+                var res = [];
+
+                return res;
+            },
+            getImageClass: function () {
+                return this.$parent.getImageClass();
+            },
+            getNumberItemsFromName: function (name) {
+                return this.$parent.getNumberItemsFromName(name);
+            },
+        },
+  components: {
+  },
+};
+</script>
